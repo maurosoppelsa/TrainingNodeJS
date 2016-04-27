930a87b84aca0800843052d40f2c9fb1200dccb1
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2016-04-27 14:15:46 -0300

    callbacks

--- exercice2/exercices-solved.js
+++ exercice2/exercices-solved.js
@@ -8,15 +8,36 @@ for(i = 0; i < 10; i++) {       <-- you shouldn't create global variables into t
 }
 
 Fix it. So it works as expected.*/
-"use strict";
-for(let i = 0; i < 10; i++) {    /* <-- The closure implies a reference to the variable, not to its value*/
+//"use strict";
+//for(let i = 0; i < 10; i++) {    /* <-- The closure implies a reference to the variable, not to its value*/
                                                         /*Solved using let declaration*/
-    setTimeout(function() { console.log(i); }, 1000);
-}
+  //  setTimeout(function() { console.log(i); }, 1000);
+//}
 /*
 2­ Write a series function that executes a list of asynchronous functions and calls a
 
 callback with the result of all of them only once they’ve all finished.
 
     Don’t use the async.js library for this. 3­ Write the same function using the events*/
+
+function firstFunction(value){
+    console.log("this is the first function " + value);
+    secondFunction(function(){
+        callTheCallBack();
+    });
+}
+
+function secondFunction(){
+    console.log("this is the second function ");
+    callTheCallBack(function(){
+        console.log("hello I'm the callback! : )");
+    });
+}
+
+function callTheCallBack(callback){
+    console.log("i will call the callback");
+    setTimeout(callback,1000);
+}
+
+firstFunction(5);
 
