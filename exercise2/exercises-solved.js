d3d08bfca19ee0ce5c618525be8d5436d2dc309e
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2016-04-27 19:15:39 -0300

    promises

--- exercise2/exercises-solved.js
+++ exercise2/exercises-solved.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 1­ What is wrong with this code?:
 
@@ -8,19 +9,19 @@ for(i = 0; i < 10; i++) {       <-- you shouldn't create global variables into t
 }
 
 Fix it. So it works as expected.*/
-//"use strict";
-//for(let i = 0; i < 10; i++) {    /* <-- The closure implies a reference to the variable, not to its value*/
+
+for(let i = 0; i < 10; i++) {    /* <-- The closure implies a reference to the variable, not to its value*/
                                                         /*Solved using let declaration*/
-  //  setTimeout(function() { console.log(i); }, 1000);
-//}
+    setTimeout(function() { console.log(i); }, 1000);
+}
 /*
 2­ Write a series function that executes a list of asynchronous functions and calls a
 
 callback with the result of all of them only once they’ve all finished.
 
-    Don’t use the async.js library for this. 3­ Write the same function using the events*/
+    Don’t use the async.js library for this. */
 
-function firstFunction(value){
+/*function firstFunction(value){
     console.log("this is the first function " + value);
     secondFunction(function(){
         callTheCallBack();
@@ -39,5 +40,32 @@ function callTheCallBack(callback){
     setTimeout(callback,1000);
 }
 
-firstFunction(5);
+firstFunction(5);*/
+
+/*3­ Write the same function using the events*/
+
+var EventEmitter = require("events").EventEmitter;
+
+var ee = new EventEmitter();
+ee.on("firstEvent", function () {
+    console.log("this is the first event");
+    ee.emit("secondEvent",function(){
+        ee.emit("callToCallback");
+    });
+});
+
+ee.on("secondEvent",function(){
+    console.log("this is the second event");
+    ee.emit("callToCallback",theCallBack);
+});
+ee.on("callToCallback",function(callback){
+    console.log("I will call a callback");
+    callback();
+});
+
+function theCallBack(){
+  console.log("I'm a callBack");
+};
+
+ee.emit("firstEvent");
 
