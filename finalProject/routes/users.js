bfc14864b7790e5a405abfb12868d4d8f5cf6860
maurosoppelsa <l.mauro.soppelsa@gmail.com>
2016-05-03 01:07:11 -0300

    stablish mongoose connection

--- finalProject/routes/users.js
+++ finalProject/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/userdb');
+mongoose.model('users',{name:String});
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    mongoose.model('users').find(function(err,users){
+      res.send(users);
+    });
 });
 
 module.exports = router;
